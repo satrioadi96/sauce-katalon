0983bcb6d5b63f5227b8edb14caa627a009e1aff
Satrio Adi Prawiro <prawiro.96@gmail.com>
2022-12-12 16:33:29 +0700

    initiate new testcase for saucedemo web ui testing

--- Test Suites/Test_SD_Login.ts
+++ Test Suites/Test_SD_Login.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Test_Login</name>
+   <name>Test_SD_Login</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -12,20 +12,13 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>d1a9ff22-69df-45ac-b353-974758039ee1</testSuiteGuid>
    <testCaseLink>
-      <guid>5ac6461e-d8be-4787-9e53-b53b8e16dc2a</guid>
+      <guid>8d8020d4-3615-44f5-99de-6932cdf6e3db</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <iterationNameVariable>
-         <defaultValue>''</defaultValue>
-         <description></description>
-         <id>19fe967b-1564-4705-aff2-848d45b84489</id>
-         <masked>false</masked>
-         <name>Username</name>
-      </iterationNameVariable>
-      <testCaseId>Test Cases/Common Test Cases/Login</testCaseId>
+      <testCaseId>Test Cases/SatrioAdiPrawiro-Quiz6/SAP6-02</testCaseId>
       <testDataLink>
          <combinationType>ONE</combinationType>
-         <id>98f65134-8ce1-4e27-bb64-253baae81430</id>
+         <id>23e68db5-da5a-484f-99ac-780464dd0da0</id>
          <iterationEntity>
             <iterationType>ALL</iterationType>
             <value></value>
@@ -34,16 +27,16 @@
       </testDataLink>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
-         <testDataLinkId>98f65134-8ce1-4e27-bb64-253baae81430</testDataLinkId>
+         <testDataLinkId>23e68db5-da5a-484f-99ac-780464dd0da0</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>Username</value>
-         <variableId>19fe967b-1564-4705-aff2-848d45b84489</variableId>
+         <variableId>3e52a56e-e422-47bd-8223-9f76918f35ba</variableId>
       </variableLink>
       <variableLink>
-         <testDataLinkId>98f65134-8ce1-4e27-bb64-253baae81430</testDataLinkId>
+         <testDataLinkId>23e68db5-da5a-484f-99ac-780464dd0da0</testDataLinkId>
          <type>DATA_COLUMN</type>
          <value>Password</value>
-         <variableId>75fa3d79-7e92-4d55-b359-f7c9493ea288</variableId>
+         <variableId>8680fd18-95b9-4ce9-acb3-0a3284ede54f</variableId>
       </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
